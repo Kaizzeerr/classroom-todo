230bc8926958f3f6308885efa4d3461829bf6a32
KaiKai <ralphkyzerlaserna@gmail.com>
2026-01-17 00:04:43 +0800

    Update subjects.js

--- js/subjects.js
+++ js/subjects.js
@@ -4,31 +4,40 @@ const subjects = JSON.parse(localStorage.getItem('subjects')) || {
   English: []
 };
 
-let currentSubject = 'Math';
+let currentSubject = Object.keys(subjects)[0];
 
 function saveSubjects() {
   localStorage.setItem('subjects', JSON.stringify(subjects));
 }
 
 function addSubject() {
-  if (!newSubject.value) return;
+  if (!newSubject.value || subjects[newSubject.value]) return;
+
   subjects[newSubject.value] = [];
+  currentSubject = newSubject.value;
   newSubject.value = '';
+
   saveSubjects();
   log('Added subject');
   renderSubjects();
+  renderTodos();
 }
 
 function deleteSubject(name) {
   if (!confirm('Delete subject?')) return;
+
   delete subjects[name];
+  currentSubject = Object.keys(subjects)[0];
+
   saveSubjects();
   log('Deleted subject');
   renderSubjects();
+  renderTodos();
 }
 
 function renderSubjects() {
   subjectTabs.innerHTML = '';
+
   Object.keys(subjects).forEach(s => {
     const btn = document.createElement('button');
     btn.textContent = s;
