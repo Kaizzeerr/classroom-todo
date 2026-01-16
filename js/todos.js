50afa178323fcf75df33624e63d00361fbd36e0c
KaiKai <ralphkyzerlaserna@gmail.com>
2026-01-16 23:14:32 +0800

    Update todos.js

--- js/todos.js
+++ js/todos.js
@@ -1,1 +1,19 @@
+function addTodo() {
+const text = todoText.value;
+if (!text) return;
+subjects[currentSubject].push({ text, icon: todoIcon.value });
+todoText.value = '';
+saveSubjects();
+renderTodos();
+}
 
+
+function renderTodos() {
+const ul = document.getElementById('todoList');
+ul.innerHTML = '';
+subjects[currentSubject].forEach(t => {
+const li = document.createElement('li');
+li.textContent = `${t.icon} ${t.text}`;
+ul.appendChild(li);
+});
+}
