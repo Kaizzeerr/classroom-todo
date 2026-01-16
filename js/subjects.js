fc34516dad7fb319874f080fd3710e065c1f813d
KaiKai <ralphkyzerlaserna@gmail.com>
2026-01-16 23:14:09 +0800

    Update subjects.js

--- js/subjects.js
+++ js/subjects.js
@@ -1,1 +1,23 @@
+const subjects = JSON.parse(localStorage.getItem('subjects')) || {
+Math: [], Science: [], English: []
+};
 
+
+let currentSubject = 'Math';
+
+
+function saveSubjects() {
+localStorage.setItem('subjects', JSON.stringify(subjects));
+}
+
+
+function renderSubjects() {
+const c = document.getElementById('subjectTabs');
+c.innerHTML = '';
+Object.keys(subjects).forEach(s => {
+const b = document.createElement('button');
+b.textContent = s;
+b.onclick = () => { currentSubject = s; renderTodos(); };
+c.appendChild(b);
+});
+}
