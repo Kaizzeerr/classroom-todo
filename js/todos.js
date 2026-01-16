392439c2ab215a2820260f6e614baac63a54603c
KaiKai <ralphkyzerlaserna@gmail.com>
2026-01-17 00:02:58 +0800

    Update todos.js

--- js/todos.js
+++ js/todos.js
@@ -1,19 +1,48 @@
 function addTodo() {
-const text = todoText.value;
-if (!text) return;
-subjects[currentSubject].push({ text, icon: todoIcon.value });
-todoText.value = '';
-saveSubjects();
-renderTodos();
+  if (!todoTitle.value) return alert('Task title required');
+
+  subjects[currentSubject].push({
+    title: todoTitle.value,
+    due: todoDue.value,
+    notes: todoNotes.value,
+    icon: todoIcon.value
+  });
+
+  todoTitle.value = '';
+  todoDue.value = '';
+  todoNotes.value = '';
+
+  saveSubjects();
+  log('Added task');
+  renderTodos();
 }
 
+function deleteTodo(index) {
+  subjects[currentSubject].splice(index, 1);
+  saveSubjects();
+  log('Deleted task');
+  renderTodos();
+}
 
 function renderTodos() {
-const ul = document.getElementById('todoList');
-ul.innerHTML = '';
-subjects[currentSubject].forEach(t => {
-const li = document.createElement('li');
-li.textContent = `${t.icon} ${t.text}`;
-ul.appendChild(li);
-});
+  todoList.innerHTML = '';
+
+  subjects[currentSubject]?.forEach((t, i) => {
+    const li = document.createElement('li');
+
+    li.innerHTML = `
+      ${t.icon} <strong>${t.title}</strong><br>
+      ${t.due ? 'Due: ' + t.due + '<br>' : ''}
+      ${t.notes || ''}
+    `;
+
+    if (users[currentUser].role !== 'viewer') {
+      const del = document.createElement('button');
+      del.textContent = 'Delete';
+      del.onclick = () => deleteTodo(i);
+      li.appendChild(del);
+    }
+
+    todoList.appendChild(li);
+  });
 }
